80182dd8b6fdcd945d50f984e1055f61e874bbd0
Nicholas Mink <nick.mink@callahorn.com>
2020-03-07 09:03:49 -0600

    Chore/eslint cleanup (#5)
    * Refactor and cleanup
    
    * Adding node & npm references to package.json

--- src/templates/PatternLibrary.js
+++ src/templates/PatternLibrary.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Main from '../components/layouts/Main'
-import Header from '../components/layouts/Header'
-import Sidebar from '../components/Sidebar/Sidebar'
-import Button from '../components/Button/Button'
-import InputText from '../components/Input/InputText'
-import Accordion from '../components/Accordion/Accordion'
-import Toggle from '../components/Toggle/Toggle'
-import InputCheckbox from '../components/Input/InputCheckbox'
-import InputSelect from '../components/Input/InputSelect'
-import { COMPONENTS_LIST, SELECT_ITEMS, CARD_CONTENT } from '../constants/pattern-library'
-import Card from '../components/Card/Card'
-import Text from '../components/Text/Text'
+import Main from '../components/layouts/Main';
+import Header from '../components/layouts/Header';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Button from '../components/Button/Button';
+import InputText from '../components/Input/InputText';
+import Accordion from '../components/Accordion/Accordion';
+import Toggle from '../components/Toggle/Toggle';
+import InputCheckbox from '../components/Input/InputCheckbox';
+import InputSelect from '../components/Input/InputSelect';
+import { COMPONENTS_LIST, SELECT_ITEMS, CARD_CONTENT } from '../constants/pattern-library';
+import Card from '../components/Card/Card';
+import Text from '../components/Text/Text';
 import Map from '../components/Map/Map';
-import { Layer, Feature } from 'react-mapbox-gl'
+import { Layer, Feature } from 'react-mapbox-gl';
 import { MAP_DEFAULT } from '../constants/map';
 import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs';
 
@@ -29,21 +29,15 @@ function PatternLibrary() {
   const handleToggle = () => setToggleStatus(!toggleStatus);
   const handleCheckbox = () => setCheckboxStatus(!checkboxStatus);
   const handleSelect = e => setSelectInput(e.target.id);
-  const notify = () => toast("Primary toast message", { position: toast.POSITION.BOTTOM_RIGHT });
+  const notify = () => toast('Primary toast message', { position: toast.POSITION.BOTTOM_RIGHT });
   const COMPONENTS = {
     text: {
       title: 'text',
       component: (
         <div>
-          <Text size="large">
-            Large text
-          </Text>
-          <Text>
-            Default text
-          </Text>
-          <Text size="small">
-            Small text
-          </Text>
+          <Text size="large">Large text</Text>
+          <Text>Default text</Text>
+          <Text size="small">Small text</Text>
           <Text size="small" uppercase>
             Small uppercase text
           </Text>
@@ -51,7 +45,7 @@ function PatternLibrary() {
             Small text with letter spacing uppercase, and bold
           </Text>
         </div>
-      )
+      ),
     },
     button: {
       title: 'Primary Button',
@@ -64,7 +58,13 @@ function PatternLibrary() {
     'input text': {
       title: 'input text',
       component: (
-        <InputText inputValue={inputValue} onChange={e => handleInput(e)} placeholder="Single input text..." label="Input Text" labelPosition="top" />
+        <InputText
+          inputValue={inputValue}
+          onChange={e => handleInput(e)}
+          placeholder="Single input text..."
+          label="Input Text"
+          labelPosition="top"
+        />
       ),
     },
     accordion: {
@@ -75,31 +75,37 @@ function PatternLibrary() {
           content={<p>Accordion content lives here...</p>}
           defaultOpen={false}
         />
-      )
+      ),
     },
     toggle: {
       title: 'toggle',
-      component: (
-        <Toggle isActive={toggleStatus} onClick={() => handleToggle()} />
-      )
+      component: <Toggle isActive={toggleStatus} onClick={() => handleToggle()} />,
     },
     checkbox: {
       title: 'checkbox',
       component: (
-        <InputCheckbox label="Input Checkbox" isActive={checkboxStatus} onChange={() => handleCheckbox()} />
-      )
+        <InputCheckbox
+          label="Input Checkbox"
+          isActive={checkboxStatus}
+          onChange={() => handleCheckbox()}
+        />
+      ),
     },
     'select input': {
       title: 'select input',
       component: (
-        <InputSelect isActive={selectInput} label="Select Input" handleSelect={e => handleSelect(e)} items={SELECT_ITEMS} placeholder="Select..." />
-      )
+        <InputSelect
+          isActive={selectInput}
+          label="Select Input"
+          handleSelect={e => handleSelect(e)}
+          items={SELECT_ITEMS}
+          placeholder="Select..."
+        />
+      ),
     },
     card: {
       title: 'card',
-      component: (
-        <Card card={CARD_CONTENT} accentHeader />
-      )
+      component: <Card card={CARD_CONTENT} accentHeader />,
     },
     toast: {
       title: 'toast',
@@ -110,11 +116,11 @@ function PatternLibrary() {
           </Button>
           <ToastContainer />
         </div>
-      )
+      ),
     },
     breadcrumbs: {
       title: 'breadcrumbs',
-      component: <Breadcrumbs url="components/breadcrumbs" />
+      component: <Breadcrumbs url="components/breadcrumbs" />,
     },
     map: {
       title: 'map',
@@ -124,23 +130,24 @@ function PatternLibrary() {
             <Feature coordinates={MAP_DEFAULT.center} />
           </Layer>
         </Map>
-      )
-    }
+      ),
+    },
   };
   return (
     <div className="app-wrapper">
       <Header title="Pattern Library" />
       <div className="app-wrapper--content">
-        <Sidebar activeItem={activeComponent} sidebarItems={COMPONENTS_LIST} onClick={e => handleComponent(e)} />
+        <Sidebar
+          activeItem={activeComponent}
+          sidebarItems={COMPONENTS_LIST}
+          onClick={e => handleComponent(e)}
+        />
         <Main activeComponent={COMPONENTS[activeComponent].component} />
       </div>
     </div>
-  )
+  );
 }
 
-PatternLibrary.propTypes = {
+PatternLibrary.propTypes = {};
 
-}
-
-export default PatternLibrary
-
+export default PatternLibrary;
